0be45d709173c383b71c14550d0be4b44efb9e51
Sameliuk <sameliuk.anastasia@gmail.com>
2025-02-28 11:24:00 +0200

    1

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/user.controller');
-
-router.post('/', function (req, res, next) {
-    res.send('respond with a resource');
-});
-
-module.exports = router;
